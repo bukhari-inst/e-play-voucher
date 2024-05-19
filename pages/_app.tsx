ff4aca6c46a965f0e7fbe690052dc69e6b86dabd
Bukhari_Inst <bukhari414647@gmail.com>
2024-05-19 10:07:50 +0700

    feat: :sparkles: "Added imports for transactions, transactions-detail, and edit-profile CSS files"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,9 @@ import '../styles/sign-up-success.css';
 import '../styles/404-not-found.css';
 import '../styles/overview.css';
 import '../styles/sidebar.css';
+import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
+import '../styles/edit-profile.css';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
